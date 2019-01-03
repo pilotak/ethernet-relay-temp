1f1e2d1ea1eaf88f608d2cdcb560472b43f718b5
Pavel Slama <p.slama@atlas.cz>
2019-01-03 20:51:41 +0100

    settings example

--- ethernet_relay_temp/ethernet_relay_temp.ino
+++ ethernet_relay_temp/ethernet_relay_temp.ino
@@ -20,7 +20,7 @@ SOFTWARE.
 
 #include <climits>
 #include <watchdog.h>
-#include "settings.h"
+#include "settings.example.h"
 
 bool sendData(const char * topic, const char * data, bool retain = false);  // compiler workaround
 
